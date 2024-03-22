4515e8a59fa424f58891abb6b4d0ba6ba3e143c2
Cheruiyot Robert <kiruir38@gmail.com>
2024-03-22 17:28:46 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+  return customerName.toUpperCase();
+}var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+function changeLeastFavoriteCustomer() {
+    throw new Error("Assignment to constant variable.");
+}
